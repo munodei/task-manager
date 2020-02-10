e72219ab69ee9361f15505e8676b7da5aed48c0d
Terrence Munodei <terrencemunodei25@gmail.com>
2020-02-10 20:25:58 +0200

    Addition of helpers

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -24,6 +24,7 @@ class ComposerStaticInit0de2b3e607f92a94a52949a949076e4c
         '801c31d8ed748cfa537fa45402288c95' => __DIR__ . '/..' . '/psy/psysh/src/functions.php',
         'b6ec61354e97f32c0ae683041c78392a' => __DIR__ . '/..' . '/scrivo/highlight.php/HighlightUtilities/functions.php',
         'ed962a97bd972bc82007176b647d4e36' => __DIR__ . '/..' . '/facade/ignition/src/helpers.php',
+        '17d016dc52a631c1e74d2eb8fdd57342' => __DIR__ . '/..' . '/laravel/helpers/src/helpers.php',
     );
 
     public static $prefixLengthsPsr4 = array (
